8a0dc3c97a6440820e41f635b768fa5dbdeef2b5
Scotty Brown <scottybrown98@gmail.com>
2023-08-27 00:11:51 -0500

    Add Header component  and styling

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Header.css"
+
+function Header() {
+    return (
+        <header className="header">
+            <h1>Rancid T<span>🍅</span>matill<span>🍅</span>'s</h1>
+        </header>
+    )
+}
+
+export default Header
