75ba43826287fcdad42cb30be28e802b6b4dfd84
Scotty Brown <scottybrown98@gmail.com>
2023-08-27 17:52:12 -0500

    Add PropType check for SingleMovie component

--- src/Components/SingleMovie/SingleMovie.js
+++ src/Components/SingleMovie/SingleMovie.js
@@ -1,3 +1,4 @@
+import PropTypes from 'prop-types'
 import './SingleMovie.css'
 import SingleMovieCard from '../SingleMovieCard/SingleMovieCard'
 
@@ -16,3 +17,18 @@ function SingleMovie({movie, goBack, displayVideo}) {
 }
 
 export default SingleMovie
+
+SingleMovie.propTypes = {
+  movies: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number,
+      poster_path: PropTypes.string,
+      backdrop_path: PropTypes.string,
+      title: PropTypes.string,
+      average_rating: PropTypes.number,
+      release_date: PropTypes.string,
+    })
+  ),
+  goBack: PropTypes.func.isRequired,
+  displayVideo: PropTypes.func.isRequired
+};
