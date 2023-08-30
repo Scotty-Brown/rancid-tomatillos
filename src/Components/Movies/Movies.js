fb4142aeeef72dd956d1df3b0bcb5c3bdd3497e5
Judy Ye <judyy.ye@gmail.com>
2023-08-30 16:32:37 -0700

    Add router to app

--- src/Components/Movies/Movies.js
+++ src/Components/Movies/Movies.js
@@ -1,29 +1,25 @@
-import MovieCard from '../MovieCard/MovieCard'
-import './Movies.css'
+import { Link, useParams } from 'react-router-dom';
+import MovieCard from '../MovieCard/MovieCard';
+import './Movies.css';
 
-function Movies({movies, showSingleMovie}) {
-  let movieCards
-  if (!movies.movies) {
-    return
-  } else {
-    movieCards = movies.movies.map(movie => {
-    return (
-      <MovieCard 
-        key={movie.id}
-        id={movie.id}
-        title={movie.title}
-        img={movie.poster_path}
-        rating={movie.average_rating}
-        showSingleMovie={showSingleMovie}
-      />
-    )
-  })
-}
-  return (
-    <div className='movies-container'>
-      {movieCards}
-    </div>
-  )
+function Movies({ movies, showSingleMovie }) {
+  const movieCards = movies.movies
+    ? movies.movies.map((movie) => {
+        return (
+          <Link to={`/movie/${movie.id}`} key={movie.id}>
+            <MovieCard
+              id={movie.id}
+              title={movie.title}
+              img={movie.poster_path}
+              rating={movie.average_rating}
+              showSingleMovie={showSingleMovie}
+            />
+          </Link>
+        );
+      })
+    : null;
+
+  return <div className="movies-container">{movieCards}</div>;
 }
 
-export default Movies
+export default Movies;
