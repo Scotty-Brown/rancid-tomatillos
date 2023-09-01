04b4b6d69e89194a2e0ab263ed7e192b4266d922
Judy Ye <judyy.ye@gmail.com>
2023-09-01 13:52:43 -0700

    Add loading state

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -6,11 +6,13 @@ import { useEffect, useState } from 'react';
 import { getAllMovies } from '../../apiCalls'
 import ErrorHandling from '../ErrorHandling/ErrorHandling'
 import { Route, Routes, useLocation } from 'react-router-dom';
+import Loading from '../Loading/Loading';
 
 function App() {
   const [movies, setMovies] = useState({})
   const [error, setError] = useState('')
   const location = useLocation().pathname
+  const [loading, setLoading] = useState(false)
 
   useEffect(() => {
     setError('')
@@ -18,15 +20,24 @@ function App() {
 
 
   useEffect(() => {
+    setLoading(true)
     getAllMovies()
-    .then(data => setMovies(data))
-    .catch(error => setError(`Request failed - ${error.message}`))
+    .then(data => {
+      setMovies(data)
+      setLoading(false)
+    })
+    .catch(error => {
+      setError(`Request failed - ${error.message}`)
+      setLoading(false)
+    })
+    
   }, [])
 
   return (
     <main>
       <Header />
       {error && <ErrorHandling error={error}/>}
+      {loading && <Loading loading={loading}/>}
       <Routes>
         <Route path='/' element={<Movies movies={movies} />} />
         <Route path='/:id' element={<SingleMovie setError={setError} />}/> 
