2505a23b5867c5ded9c41ed103cce1bb4d3d0fb5
Scotty Brown <scottybrown98@gmail.com>
2023-09-02 15:50:07 -0500

    Refactor SingleMovieCard prop and update Proptypes

--- src/Components/SingleMovieCard/SingleMovieCard.js
+++ src/Components/SingleMovieCard/SingleMovieCard.js
@@ -1,31 +1,33 @@
 import { Link } from 'react-router-dom'
 import './SingleMovieCard.css'
 
-function SingleMovieCard({title, tagLine, releaseDate, genres, img, backdropImg, rating, budget, revenue, runTime, overview }) {
+function SingleMovieCard({ singleMovie }) {
 
   return (
-    <article className='single-movie-card' style={{'--backdrop-img': `url(${backdropImg})`}}> 
+    <article className='single-movie-card' style={{'--backdrop-img': `url(${singleMovie.movie.backdrop_path})`}}> 
       <div className='single-card-body-all'>
-          <img className='single-movie-card-image' src={img} alt={title}/>
+          <img className='single-movie-card-image' src={singleMovie.movie.poster_path} alt={singleMovie.movie.title}/>
           <section className='single-card-body-text'>
             <div className='single-movie-headings'>
-              <h2>{title}</h2>
-              <h3>Tagline: "{tagLine} "</h3>
+              <h2>{singleMovie.movie.title}</h2>
+              <h3>Tagline: "{singleMovie.movie.tagline} "</h3>
             </div>
             <div className='single-card-description'>
-              <p>Release Date: {releaseDate}</p>
-              <p>Genres: {genres?.join(' | ').split()}</p>
-              <p>Rancid Rating - {rating?.toFixed(1)} 🍅s</p>
-              <p>Budget: ${budget?.toLocaleString()}</p>
-              <p>Revenue: ${revenue?.toLocaleString()}</p>
-              <p>Run Time: {runTime} minutes</p>
-              <p>{overview}</p>
+              <p>Release Date: {singleMovie.movie.release_date}</p>
+              <p>Genres: {singleMovie.movie.genres?.join(' | ').split()}</p>
+              <p>Rancid Rating - {singleMovie.movie.average_rating?.toFixed(1)} 🍅s</p>
+              <p>Budget: ${singleMovie.movie.budget?.toLocaleString()}</p>
+              <p>Revenue: ${singleMovie.movie.revenue?.toLocaleString()}</p>
+              <p>Run Time: {singleMovie.movie.runtime} minutes</p>
+              <p>{singleMovie.movie.overview}</p>
               <Link to={'/'} className='button'>Go Back</Link>
             </div>
           </section>
       </div>
-    </article>
+     </article>
   )
 }
 
 export default SingleMovieCard
+
+
