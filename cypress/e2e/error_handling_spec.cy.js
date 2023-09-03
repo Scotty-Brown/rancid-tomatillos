f486893ff335fb33adebde7f27321d59697719a6
Judy Ye <judyy.ye@gmail.com>
2023-09-03 15:01:33 -0700

    Refactor test to move intercepts into beforeEaches

--- cypress/e2e/error_handling_spec.cy.js
+++ cypress/e2e/error_handling_spec.cy.js
@@ -1,49 +1,82 @@
 describe('Error Handling Page', () => {
-  it('Should select the first movie and show 404 error', () => {
-    cy.intercept(
+  beforeEach(() => {
+    cy.intercept('GET', 'https://rancid-tomatillos.herokuapp.com/api/v2/movies', {
+      statusCode: 200,
+      fixture: 'mainSampleData'
+    }).as('getData')
+
+   cy.intercept(
       'GET',
-      'https://rancid-tomatillos.herokuapp.com/api/v2/movies',
-      {
-        statusCode: 200,
-        fixture: 'mainSampleData'
-      }).as('getData')
-      .visit('http://localhost:3000/')
-      .wait('@getData')
-    
-      cy.intercept(
-      'GET',
-      'https://rancid-tomatillos.herokuapp.com/api/v2/movies/436270',
-      { statusCode: 404 }
-    ).as('getSecondData')
+      'https://rancid-tomatillos.herokuapp.com/api/v2/movies/436270',{ 
+        statusCode: 404,
+        fixture: 'blackAdamMock'
+       }
+    ).as('getBlackAdam')
 
     cy.intercept('GET', 'https://rancid-tomatillos.herokuapp.com/api/v2/movies/436270/videos', {
-      statusCode: 200, 
-      fixture: 'trailerFetch'
-    }).as('trailer')
+      statusCode: 404, 
+      fixture: 'blackAdamTrailerFetch'
+    }).as('getBlackAdamTrailer') 
+    
    
+  });
 
-    cy.get('[href="/436270"] > .movie-card').click()
-      cy.wait(['@getSecondData', '@trailer'])
-      .url().should('eq', 'http://localhost:3000/436270')
+  it ('should return a 404 error for Black Adam' , () => {
+    cy.visit('http://localhost:3000/')
+    cy.wait('@getData')
+      cy.get('[href="/436270"] > .movie-card').click()
+    cy.wait(['@getBlackAdamTrailer', '@getBlackAdam'])
+    .url().should('eq', 'http://localhost:3000/436270')
+    .get('.error').should('exist')
+    .get('h2').should('contain', 'Request failed - 404: Nothing to see here')
+    .get('img').should('have.attr', 'src');
+  })
+   
+  it('should return all movies', () => {
+    cy.visit('http://localhost:3000/436270')
+    cy.wait(['@getBlackAdamTrailer', '@getBlackAdam'])
+    cy.get('.retry-button').click()
+    .wait('@getData')
+    .url().should('eq', 'http://localhost:3000/')
+    .get('.movie-card').first().contains('p', 'Rancid Rating - 4.0 🍅s')
+    .get('.movie-card-image').first().should(
+      'have.attr',
+      'src',
+      'https://image.tmdb.org/t/p/original//pFlaoHTZeyNkG83vxsAJiGzfSsa.jpg'
+    )
+    .get('.movie-card').last().contains('p', 'Rancid Rating - 7.0 🍅s')
+    .get('.movie-card-image').last().should(
+      'have.attr',
+      'src',
+      'https://image.tmdb.org/t/p/original//g4yJTzMtOBUTAR2Qnmj8TYIcFVq.jpg'
+    )
+    .get('.movies-container').find('.movie-card').should('have.length', 3);
+  });
+})
+    
+describe('400 error', () => {
+  it('should return 404 error for all movies page', () => { 
+     cy.intercept('GET', 'https://rancid-tomatillos.herokuapp.com/api/v2/movies', {
+      statusCode: 404,
+      fixture: 'mainSampleData'
+    }).as('get404FetchError')
+      cy.visit('http://localhost:3000/')
+      .wait('@get404FetchError')
       .get('.error').should('exist')
       .get('h2').should('contain', 'Request failed - 404: Nothing to see here')
-      .get('img').should('have.attr', 'src');
+      .get('img').should('have.attr', 'src')
+    });
+})
 
-    cy.get('a').click()
-      .wait('@getData')
-      .url().should('eq', 'http://localhost:3000/')
-      .get('.movie-card').first().contains('p', 'Rancid Rating - 4.0 🍅s')
-      .get('.movie-card-image').first().should(
-        'have.attr',
-        'src',
-        'https://image.tmdb.org/t/p/original//pFlaoHTZeyNkG83vxsAJiGzfSsa.jpg'
-      )
-      .get('.movie-card').last().contains('p', 'Rancid Rating - 7.0 🍅s')
-      .get('.movie-card-image').last().should(
-        'have.attr',
-        'src',
-        'https://image.tmdb.org/t/p/original//g4yJTzMtOBUTAR2Qnmj8TYIcFVq.jpg'
-      )
-      .get('.movies-container').find('.movie-card').should('have.length', 3);
-  });
-});
+describe('500 error', () => {
+     it('should return 500 error for all movies page', () => {
+      cy.intercept('GET', 'https://rancid-tomatillos.herokuapp.com/api/v2/movies', {
+        statusCode: 500, fixture: 'mainSampleData'}).as('get500FetchError')
+        .visit('http://localhost:3000/')
+        .wait('@get500FetchError')
+      .get('.error').should('exist')
+      .get('h2').should('contain', 'Request failed - 500: Nothing to see here')
+      .get('img').should('have.attr', 'src')
+    })
+})
+   
